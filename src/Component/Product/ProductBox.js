dcf12450872bfd6a0e47a0ec4a7699d63848f73f
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2023-10-12 01:02:53 +0200

    new changes admin interfae

--- src/Component/Product/ProductBox.js
+++ src/Component/Product/ProductBox.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './ProductBox.css'; // Importez le fichier CSS
 
-const ProductBox = ({ imageUrl, productName, shortDescription, showDescription }) => {
+
+const ProductBox = ({ imageUrl, productName, shortDescription, showDescription, boxWidth, boxHeight }) => {
+  const squareStyle = {
+    width: boxWidth, // Utilisez "width" au lieu de "boxWidth" comme clé de style
+    height: boxHeight
+  };
+
   return (
     <div>
-      <div className="square">
+      <div className="square" style={squareStyle}>
         <img src={imageUrl} alt="Votre image" className="square-image" />
       </div>
       <div className="DescriptionStyle">
@@ -16,10 +22,13 @@ const ProductBox = ({ imageUrl, productName, shortDescription, showDescription }
 
 ProductBox.defaultProps = {
   productName: "Nom:",
+  boxWidth: "33px",
+  boxHeight: "33px",
   shortDescription: "Courte description par défaut",
   showDescription: true
 };
 
 export default ProductBox;
+
 
 
--- src/Interface/Admin.js
+++ src/Interface/Admin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../Component/Navbar/navbar';
 import ProductBox from '../Component/Product/ProductBox';
+import Button from '../Component/Button';
 
 const Admin = () => {
   return (
@@ -8,29 +9,37 @@ const Admin = () => {
       <div className="NavStyle">
         <Navbar showSearchbar={false} showWelcomeText={true} buttonColor='#46D075'/>
       </div>
-      <div className="Boxcontent" style={{marginLeft: "25%", marginTop:'5%', width:"40%", position:"fixed"}}>
+      <div className="Boxcontent" style={{marginLeft: "25%", marginTop:'5%', width:"35%", position:"fixed"}}>
         <h3>Ajoutez un nouveau produit</h3>
-        <ProductBox style={{width: "44px" }} showDescription={false}/>
+        <div className="Bocontent" style={{marginLeft: "15%"}}>
+        <ProductBox boxWidth= "80%" boxHeight="400px" showDescription={false} />
+        </div>
 
         <div className="Boxcontent" style={{ width: "100%" }}>
+        <div className="data" style={{ flexDirection: 'row', alignItems: 'center' }}>
         <label htmlFor="description">Description: </label>
-        <input type="text" id="description" name="description" style={{ width: "10%", marginLeft: "4%" }}/>
-
-        <div className="data" style={{ marginTop: "1%" }}>
-        <label htmlFor="description">Prix: </label>
-        <input type="text" id="description" name="description" style={{ width: "10%", marginLeft: "12%" }}/>
+        <input type="text" id="description" name="description" style={{ width: "50%", marginLeft: '7%' }} />
         </div>
 
-        <div className="data" style={{ marginTop: "1%" }}>
-        <label htmlFor="description">Promotion: </label>
-        
-        <input type="text" id="description" name="description" style={{ width: "10%", marginLeft: "5%"  }}/>
-        
-        </div>
-        </div>
+       <div className="data" style={{ flexDirection: 'row', alignItems: 'center', marginTop: "1%" }}>
+       <label htmlFor="prix">Prix: </label>
+      <input type="text" id="prix" name="prix" style={{ width: "50%", marginLeft: '17%' }} />
       </div>
 
-      <div className="Boxcontent" style={{marginLeft: "77%", marginTop:'3%' , width:"20%", position:"relative"}}></div>
+     <div className="data" style={{ flexDirection: 'row', alignItems: 'center', marginTop: "1%" }}>
+    <label htmlFor="promotion">Promotion: </label>
+    <input type="text" id="promotion" name="promotion" style={{ width: "50%", marginLeft: '8%' }} />
+    </div>
+    </div>
+    <Button/>
+        </div>
+
+      <div className="Boxcontent" style={{marginLeft: "77%", marginTop:'3%' , width:"15%", position:"relative"}}>
+      <h4>Produit récemment ajouté</h4>
+      <div className='recent' style={{display: 'flex', justifyContent: 'center'}}>
+      <ProductBox boxWidth= "90%" boxHeight="150px" />
+      </div>
+      </div>
     </div>
   );
 };
